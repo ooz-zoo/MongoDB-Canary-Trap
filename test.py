d65283d959c2d2efcfb20dcf881611ec66c98a7c
JoyMicheni <111248086+ooz-zoo@users.noreply.github.com>
2025-04-29 21:06:43 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pymongo
-
-client = pymongo.MongoClient(os.environ['CHANGE_STREAM_DB'])
-#client.changestream.collection.insert_one({"_id":1, "hallo":"schnitzel"})
-client.changestream.collection.update_one({"_id":1}, {"$set": {"elon": "mars"}})
-#client.changestream.collection.replace_one({"_id":1}, {"bye": "world"})
-#client.changestream.collection.delete_one({"_id":1})
